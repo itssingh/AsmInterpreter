68202eead7ba3b8dbe13d2c90648c5b8531d46d1
Sarita Singh <saritasingh.0425@gmail.com>
2021-08-04 14:07:14 +0530

    Remove redundant files and update interpreter
    Signed-off-by: Sarita Singh <saritasingh.0425@gmail.com>

--- main.cpp
+++ main.cpp
@@ -7,12 +7,16 @@ int main()
 {
 	AsmI asmi;
 	asmi.printHelp();
+	asmi.printRegisters();
 	while (1)
 	{
 		std::string line;
 		std::getline(std::cin, line);
+		if(line == "run"){
+			break;
+		}
 		asmi.handleInput(line);
 		asmi.command();
-		asmi.printRegisters();
 	}
+	asmi.printRegisters();
 }
